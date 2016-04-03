508dbaa48d3f4cb7eb054303fcf9811e310d2cfb
slarribeau <slarribeau@gmail.com>
2016-04-02 19:33:43 -0700

    print sorted list, after dups are removed

--- partial_dictionary.py
+++ partial_dictionary.py
@@ -1,18 +1,19 @@
 import re, collections
 import urllib2
 
-def words(text): return re.findall('[a-z]+', text.lower())
+def words(text): return re.findall('[a-z]+', text.lower());
 
 myurl = 'http://norvig.com/big.txt';
 hdr = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
-request = urllib2.Request(myurl, headers=hdr)
-result = urllib2.urlopen(request)
-the_page = result.read()
+request = urllib2.Request(myurl, headers=hdr);
+result = urllib2.urlopen(request);
+the_page = result.read();
 
 
 
 
-dupList = words(the_page)
-noDupList=list(set(dupList))
-print(noDupList);
+dupList = words(the_page);
+noDupList=list(set(dupList));
+sortedNoDupList = sorted(noDupList);
+print(sortedNoDupList);
 
