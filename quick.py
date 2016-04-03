c7e459453ba1441d241ed1d7670dd9e639f872fe
slarribeau <slarribeau@gmail.com>
2016-04-03 14:45:46 -0700

    complete rest or program

--- quick.py
+++ quick.py
@@ -1,23 +1,28 @@
-def swap(A, a, b):
-        print(a, b)
-        tmp = A[a];
-        A[a] = A[b];
-        A[b] = tmp;
-    
-def partition(A, lo, hi) :
-   piv = A[hi];
-   j = lo;
-   i = lo;
-   while j < (hi)  :
-     print j, hi, hi-1;
-     if A[j] <= piv :
-        swap(A, i, j);
-        i = i + 1;
-     j = j + 1;
-   swap(A, i, hi); 
-   return i;
+def quick(data, start, end):
+   if (start >= end):
+      return;
+   mark = partition(data, start, end);
+   quick(data, start, mark);
+   quick(data, mark+1, end);
 
-arr=[9,6,4,3,2,1,5];
-partition(arr, 0, 6);
-print(arr);
+def partition(data, start, end):
+   pivot=data[end-1];
+   i=start;
+   j=start;
 
+   while(i<(end-1)):
+     if data[i] <= pivot:
+        swap(data,i,j);
+        j=j+1;
+     i=i+1;
+   swap(data, j, end-1);
+   return j;
+
+def swap(data, a, b):
+   tmp=data[a];
+   data[a]=data[b];
+   data[b]=tmp;
+
+data=['a','x', 'm', 'q', 'r', 't', 'u', 'b'];
+quick(data,0,8);
+print(data);
