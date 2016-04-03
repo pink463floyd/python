3cd1c20bbd6d3e704dbef4e326dc67b05cf72c83
slarribeau <slarribeau@gmail.com>
2016-04-02 19:46:17 -0700

    accept URL as parameter

--- partial_dictionary.py
+++ partial_dictionary.py
@@ -1,16 +1,21 @@
 import re, collections
 import urllib2
+import sys
 
-def words(text): return re.findall('[a-z]+', text.lower());
+def words(text): 
+   return re.findall('[a-z]+', text.lower());
 
-myurl = 'http://norvig.com/big.txt';
+
+if len(sys.argv) == 1: 
+   print("usage: %s http://norvig.com/big.txt" % (sys.argv[0]));
+   sys.exit(0);
+
+myurl = (str(sys.argv[1]))
+
 hdr = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
 request = urllib2.Request(myurl, headers=hdr);
 result = urllib2.urlopen(request);
 the_page = result.read();
-
-
-
 
 dupList = words(the_page);
 noDupList=list(set(dupList));
