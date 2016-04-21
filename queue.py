adbece4746528c9cab4cfd4ebe2d748cfa09a94c
slarribeau <slarribeau@gmail.com>
2016-04-21 11:56:38 -0700

    init checkin

---
+++ queue.py
@@ -0,0 +1,6 @@
+from collections import deque
+q=deque([]);
+q.append(0);
+q.append(1);
+print(q.popleft());
+
---
+++ stack.py
@@ -0,0 +1,4 @@
+stack=[];
+stack.append('a');
+stack.append('b');
+print(stack.pop());
