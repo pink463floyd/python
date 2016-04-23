1c85f85ff40b23d268089bf14b048ab6a08bb90c
slarribeau <slarribeau@gmail.com>
2016-04-23 10:50:49 -0700

    prune, don\'t add visited node to stack

--- dfs_loop_graph.py
+++ dfs_loop_graph.py
@@ -1,29 +1,24 @@
-graph = {'A': set(['B', 'C']),
- 'B': set(['A', 'D', 'E']),
- 'C': set(['A', 'F']),
- 'D': set(['B']),
- 'E': set(['B', 'F']),
- 'F': set(['C', 'E'])
+G = {'A': ['B', 'C'],
+     'B': ['A', 'D', 'E'],
+     'C': ['A', 'F'],
+     'D': ['B'],
+     'E': ['B', 'F'],
+     'F': ['C', 'E']
 }
 
 
-def dfs(graph, start):
-    visited, stack = set(), [start]
-    print("start with one node in stack: ", start)
-    print("start with empty visited: ", visited)
+def dfs(G, start):
+    visited = set();
+    stack=[];
+    stack.append(start);
     while stack:
-        print("This is my stack(pre-pop): ", stack);
-        vertex = stack.pop()
-        print("This is my stack(post-pop: ", stack);
-        print("This is my vertex: ", vertex);
-        print("This is my visited: ", visited);
-        if vertex not in visited:
-            visited.add(vertex)
-            stack.extend(graph[vertex] - visited)
-            print('    ');
+        v = stack.pop()
+        print(v);
+        visited.add(v)
+        x = set(G[v]) - set(stack)
+        stack.extend(x - visited)
+        #stack.extend(set(G[v]) - visited)
 
-    print("done");
-    return visited
 
-dfs(graph, 'A') # {'E', 'D', 'F', 'A', 'C', 'B'}
+dfs(G, 'A') 
 
