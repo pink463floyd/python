719c44a31bb1a444ba69af4d41a06882e9350c4d
slarribeau <slarribeau@gmail.com>
2016-04-23 11:24:39 -0700

    Write a version that does not rely on ability to compare sets

--- dfs_loop_graph.py
+++ dfs_loop_graph.py
@@ -9,16 +9,34 @@ G = {'A': ['B', 'C'],
 
 def dfs(G, start):
     visited = set();
-    stack=[];
-    stack.append(start);
+    stack=[start];
     while stack:
-        v = stack.pop()
+        v = stack.pop();
         print(v);
         visited.add(v)
         x = set(G[v]) - set(stack)
         stack.extend(x - visited)
-        #stack.extend(set(G[v]) - visited)
 
 
-dfs(G, 'A') 
+dfs(G, 'A');
+print("----");
 
+
+def dfs_fast(G, start):
+    stack=[start];
+    stack_hash = {start:None};
+    visited = set();
+    while stack:
+       v = stack.pop();
+       del stack_hash[v]
+       print(v);
+       visited.add(v);
+       #only add to stack if not visited and if not in stack.
+       for x in G[v]:
+         if x not in stack_hash: 
+           if x not in visited:
+              stack.append(x);
+              stack_hash[x]=None;
+
+
+dfs_fast(G, 'A')
