f48b71c82227e690a3fafee0a87858ba9365c49a
slarribeau <slarribeau@gmail.com>
2016-05-13 09:29:40 -0700

    init checkin

---
+++ timeTest.py
@@ -0,0 +1,15 @@
+import json
+import urllib2
+import urllib2, base64
+import time
+from pprint import pprint
+import time
+import datetime
+
+
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+tt = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+print tt
+
